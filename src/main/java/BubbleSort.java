8cde154294a240c4cbfc3d53717811d6967b4c0b
Fast-And-TheCurious <109479886+Fast-And-TheCurious@users.noreply.github.com>
2023-04-12 15:10:26 +0200

    Update BubbleSort.java

--- src/main/java/BubbleSort.java
+++ src/main/java/BubbleSort.java
@@ -29,117 +29,24 @@ public class BubbleSort {
             System.out.println(arr[j]);
         }
     }
-    public static void bubble(int[]arr){
-    boolean swapped =true;
-    int n = arr.length;
-    int i=0;
-    while(swapped){
-        swapped=false;
-        i++;
-        for (int j = 0; j < n-i; j++) {
-            if (arr[j]>arr[j+1]) {
-                int temp=arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=temp;
-                swapped=true;
-            }
-        }
-    }    
-    }
-    public static void bubble2(int[]arr){
-        boolean swapped=true;
-        int i=0;
-        int n=arr.length;
-            while(swapped){
-                swapped=false;
-                i++;
-                for (int j = 0; j < n-i; j++) {
-                    if (arr[j]>arr[j+1]) {
-                        int temp = arr[j];
-                        arr[j]=arr[j+1];
-                        arr[j+1]=temp;
-                        swapped=true;
-                    }
-                }
-            }
-    }
-    public static void bubble3(int[]arr){
-    
-            boolean swapped =true;
-            int n=arr.length;
-            int i =0;
-            while(swapped){
-                swapped=false;
-                i++;
-                for (int j = 0; j < n-i; j++) {
-                    if (arr[j]>arr[j+1]) {
-                        int temp=arr[j];
-                        arr[j]=arr[j+1];
-                        arr[j+1]=temp;
-                        swapped=true;
-                    }
-                }
-            }                   
-    }
-     private static void improvedBubbleSort(int[] array) {
-        boolean swapped = true;
-        int n = array.length;
-        int i = 0;
-        while (swapped) {
-            swapped = false;
-            i++;
-            for (int j = 0; j < n - i; j++) {
-                if (array[j] > array[j + 1]) {
-                    int temp = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = temp;
-                    swapped = true;
+   //Words Sort
+         public void bubbleSortWords() {
+        boolean swapped = true; // Initialize swapped as true
+        int n = arr.length - 1; // Get length of array
+        while (swapped) { // Begin outer loop
+            swapped = false; // Set swapped to false
+            n--; // Increment i
+            for (int j = 0; j < n; j++) { // Begin inner loop
+                if (arr[j].compareTo(arr[j + 1])>0) { // If current element is greater than next element
+                    // swap arr[j] and arr[j+1]
+                    String temp = arr[j]; // Store current element in temp variable
+                    arr[j] = arr[j + 1]; // Replace current element with next element
+                    arr[j + 1] = temp; // Replace next element with temp variable
+                    swapped = true; // Set swapped to true
                 }
             }
         }
-        for (int j = 0; j < n; j++) {
-            System.out.println(array[j]);
-        }
-    } 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+        
+    }
     
 }
