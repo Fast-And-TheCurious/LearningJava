5253dfa6597d590444b782545364816909eb718a
Fast-And-TheCurious <109479886+Fast-And-TheCurious@users.noreply.github.com>
2023-04-13 07:57:49 +0200

    Add files via upload

---
+++ src/main/java/NamesArrayUI.java
@@ -0,0 +1,10 @@
+package Practice;
+
+public class NamesArrayUI {
+    public static void main(String[] args) {
+        NamesArray names = new NamesArray();
+        System.out.println(names.toString());
+        names.sort();
+        System.out.println(names.toString());
+    }
+}
