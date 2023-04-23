2c8bf9223d5e12797d55455375e4af54f65426c9
Fast-And-TheCurious <109479886+Fast-And-TheCurious@users.noreply.github.com>
2023-04-23 04:42:36 -0700

    Update SelectionSort.java

--- src/main/java/SelectionSort.java
+++ src/main/java/SelectionSort.java
@@ -25,4 +25,24 @@ public class SelectionSort {
             System.out.println(arr[i]);
         }
     }
+    public static void selectionSortImproved(int[] arr) {
+        int n = arr.length; // Get length of array
+            for (int i = 0; i < n - 1; i++) { // Begin outer loop
+            int minIndex = i; // Set minIndex to i
+                for (int j = i; j < n; j++) { // Begin inner loop
+                    if (arr[j] < arr[minIndex]) { // If current element is less than minimum element
+                    minIndex = j; // Update minIndex to the index of the current element
+        }
+}
+// swap arr[i] and arr[minIndex] if they are different
+if (minIndex != i) {
+    int temp = arr[i]; // Store current element in temp variable
+    arr[i] = arr[minIndex]; // Replace current element with minimum element
+    arr[minIndex] = temp; // Replace minimum element with temp variable
+}
+    }
+    for (int i = 0; i < n; i++) {
+    System.out.println(arr[i]);
+}
+}
 }
